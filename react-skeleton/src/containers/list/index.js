b51d57bb57bd1a8c5cb686b4c9c6106dd68ef9b6
Nataliya Shipelova <n.shipelova@butik.ru>
2018-09-04 22:45:46 +0300

    transform objects to array and render tree

--- react-skeleton/src/containers/list/index.js
+++ react-skeleton/src/containers/list/index.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router-dom';
-import * as actions from "../../store/actions";
+import * as actions from '../../store/actions';
+import getDataFromApi from '../../utils/fakeAPI';
 
 class List extends Component {
 
@@ -11,9 +12,52 @@ class List extends Component {
     dispatch: PropTypes.func
   };
 
+  constructor() {
+    super();
+    this.buildTree = this.buildTree.bind(this);
+  }
+
+  itemsToList(items) {
+    let list = [];
+    items.sort((prev, next) => {
+      return prev._id - next._id;
+    });
+    items.map(item => {
+      let key = item.parent;
+      if (key == null) {
+        key = 0;
+      }
+      list[key] = items.filter(el => el.parent == item.parent);
+    });
+    return list;
+  }
+
+  buildTree(list, parentId) {
+    if (Array.isArray(list[parentId])) {
+      return list[parentId].map(listItem => {
+        return (
+          <ul key={listItem._id}>
+            <li>
+              {listItem.title}
+              {this.buildTree(list, listItem._id)}
+            </li>
+          </ul>
+        );
+      });
+    }
+  }
+
   render() {
+    const items = getDataFromApi();
+    const list = this.itemsToList(items);
     return (
-      <h2>Ненумерованный список (дерево) с функцией inline редактирования</h2>
+      <div>
+        <h2>Ненумерованный список (дерево) с функцией inline редактирования</h2>
+        {console.log(list)}
+        <ul>
+          {this.buildTree(list, 0)}
+        </ul>
+      </div>
     );
   }
 }
